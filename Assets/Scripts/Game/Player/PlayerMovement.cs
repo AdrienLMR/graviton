a4ed3119bb38c36fe50a15a7e433492dcb2f782b
adrienlemaire <ad.lemaire@student.isartdigital.com>
2023-04-01 20:51:22 +0200

    fix little bugs

--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -6,61 +6,64 @@ public delegate void PlayerMovementDelegate(PlayerMovement sender);
 public class PlayerMovement : Movement
 {
     [Header("Player Values")]
-	[SerializeField] private float speed = 0f;
+    [SerializeField] private float speed = 0f;
 
-	[SerializeField] private string boolAnimator = "Run";
+    [SerializeField] private string boolAnimator = "Run";
 
-	public int playerIndex = 0;
-	public Vector2 movementInput { private get; set; }
-	public Vector2 rotateInput { private get; set; }
+    public int playerIndex = 0;
+    public Vector2 movementInput { private get; set; }
+    public Vector2 rotateInput { private get; set; }
 
-	public static event PlayerMovementDelegate OnCollisionVortex;
-	[SerializeField] private Animator animator;
+    public static event PlayerMovementDelegate OnCollisionVortex;
+    [SerializeField] private Animator animator;
 
-	#region Unity Methods
-	protected override void Start()
-	{
-		SetModeMove();
-	}
-	#endregion
+    #region Unity Methods
+    protected override void Start()
+    {
+        SetModeMove();
+    }
+    #endregion
 
-	#region State Machine
-	public void SetModeDie()
-	{
-		FMODUnity.RuntimeManager.PlayOneShot("event:/CHA/sound_cha_death_a1");
-		OnCollisionVortex?.Invoke(this);
-		DoAction = DoActionDie;
-	}
+    #region State Machine
+    public void SetModeDie()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/CHA/sound_cha_death_a1");
+        OnCollisionVortex?.Invoke(this);
+        DoAction = DoActionDie;
+    }
 
-	protected override void DoActionMove()
-	{
-		velocity = speed * Time.deltaTime * movementInput;
+    protected override void DoActionMove()
+    {
+        velocity = speed * Time.deltaTime * movementInput;
 
-		if (CheckExceedLimitMap(transform.position - arena.position))
-			SlideAgainstWall(transform.position - arena.position);
+        if (CheckExceedLimitMap(transform.position - arena.position))
+            SlideAgainstWall(transform.position - arena.position);
 
-		if (movementInput.magnitude == 0f)
+        if (movementInput.magnitude == 0f)
         {
-			animator.SetBool(boolAnimator, false);
+            animator.SetBool(boolAnimator, false);
         }
         else
         {
-			animator.SetBool(boolAnimator, true);
+            animator.SetBool(boolAnimator, true);
         }
 
-		UpdatePosition();
-	}
+        UpdatePosition();
+    }
 
-	public void DoActionDie()
+    public void DoActionDie()
     {
 
     }
     #endregion
 
+    private Vector2 test = Vector2.zero;
+
     protected override void UpdatePosition()
     {
         base.UpdatePosition();
 
-		transform.rotation = Quaternion.LookRotation(Vector3.forward, rotateInput);
+        if (rotateInput != Vector2.zero && rotateInput.magnitude > 0.1f)
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, rotateInput);
     }
 }
